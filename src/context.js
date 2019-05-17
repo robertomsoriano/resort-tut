e6e5f68384041aeb0d5d9bbf373ffa6a5179d92d
Roberto <robertmsoriano@gmail.com>
2019-05-17 13:12:11 -0400

    added code missing on first commit

--- src/context.js
+++ src/context.js
@@ -58,7 +58,6 @@ export default class RoomProvider extends Component {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
-    console.log(name, value);
 
     this.setState(
       {
